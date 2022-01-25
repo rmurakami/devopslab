3707b1bd3088b95aca4db4e9e68de5473afeb0c0
rmurakami <ronaldo.mura@gmail.com>
2022-01-25 19:37:23 -0300

    fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Hello World - Ronaldo Murakami"
 
 if __name__ == '__main__':
     app.run()
